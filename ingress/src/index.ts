657718b1643194d25da3011b1f6e7d8e66b54c68
Christopher Perkins <Christopher.Perkins@null.net>
2024-03-01 18:10:10 -0500

    feat: Event signature specifies function call
    Co-authored-by: vincentsz <vincentsz@users.noreply.github.com>
    Co-authored-by: Blake Fromkin <blakefromkin@gmail.com>
    Co-authored-by: Morgan-Summer-Davis <Morgan-Summer-Davis@users.noreply.github.com>

--- ingress/src/index.ts
+++ ingress/src/index.ts
@@ -4,16 +4,23 @@ const app = ingress;
 
 const func1 = app.createFunction({
   id: 'first-function',
-  event: 'first-event',
-  fn: () => {
+  event: 'event1',
+  fn: async () => {
     console.log('Hello world');
   },
 });
 const func2 = app.createFunction({
-  id: 'first-function',
-  event: 'first-event',
-  fn: () => {
+  id: 'second-function',
+  event: 'event1',
+  fn: async () => {
     console.log('Hi :)');
+  },
+});
+const func3 = app.createFunction({
+  id: 'third-function',
+  event: 'event2',
+  fn: async () => {
+    console.log('Hey, Vinnie!');
   },
 });
 
--- ingress/src/ingress.ts
+++ ingress/src/ingress.ts
@@ -1,17 +1,28 @@
-import express from 'express';
+import express, { Request } from 'express';
+
+interface Event {
+  event: String;
+  payload: Object | undefined;
+}
+
+type EventFunction = (event: Event) => Promise<void>;
 
 interface FunctionData {
   id: string;
   event: string;
-  fn: Function;
+  fn: EventFunction;
 }
 
 const app = express();
+app.use(express.json());
 let functions: FunctionData[] = [];
 
-app.post('/event', (req, res) => {
-  functions.forEach((data) => {
-    data.fn();
+app.post('/event', (req: Request<{}, {}, Event>, res) => {
+  const funcsToExecute = functions.filter(
+    (func) => func.event === req.body.event
+  );
+  funcsToExecute.forEach((data) => {
+    data.fn(req.body);
   });
 
   res.status(200);

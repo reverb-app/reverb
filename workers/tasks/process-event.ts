29b268469e3ffa3583f257ed40efcf453b327077
Blake Fromkin <blakefromkin@gmail.com>
2024-03-05 14:08:14 -0500

    feat: Implemented process-event task executor calling functions by event, by querying event/function metadata from event_functions database
    Co-authored-by: Christopher Perkins <Christopher.Perkins@null.net>
    Co-authored-by: Morgan-Summer-Davis <Morgan-Summer-Davis@users.noreply.github.com>
    Co-authored-by: vincentsz <vincentsz@users.noreply.github.com>

--- workers/tasks/process-event.ts
+++ workers/tasks/process-event.ts
@@ -1,5 +1,11 @@
 import { Task } from "graphile-worker";
+import { Pool } from "pg";
 
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  database: "event_functions",
+});
 interface Event {
   name: string;
   payload?: unknown;
@@ -15,11 +21,36 @@ const isValidEvent = (event: unknown): event is Event => {
 };
 
 const process_event: Task = async function (event, helpers) {
+  const client = await pool.connect();
+
   if (!isValidEvent(event)) {
     return;
   }
 
-  console.log(event.name);
+  try {
+    const names = (
+      await client.query(
+        `SELECT functions.name FROM functions JOIN events ON functions.event_id = events.id WHERE events.name = $1;`,
+        [event.name]
+      )
+    ).rows.map(obj => obj.name);
+    console.log(names);
+    names.forEach(funcId => {
+      fetch(`${"http://localhost:3002"}/calls`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          method: funcId,
+          params: { event },
+        }),
+      });
+    });
+  } finally {
+    client.release();
+  }
 };
 
 export default process_event;

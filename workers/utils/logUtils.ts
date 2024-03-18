70e5c0d7ac2685774ae8bef051f24b28f672663f
Christopher Perkins <Christopher.Perkins@null.net>
2024-03-18 00:19:13 -0400

    feat: hooked up cdk mongodb to workers server

--- workers/utils/logUtils.ts
+++ workers/utils/logUtils.ts
@@ -1,16 +1,24 @@
-import winston from 'winston';
-import 'winston-mongodb';
+import winston from "winston";
+import "winston-mongodb";
 
 const { combine, timestamp, json, simple } = winston.format;
 
+let mongoConnectionString = process.env.MONGO_CONNECTION_STRING as string;
+const secret = process.env.MONGO_SECRET;
+if (secret) {
+  const url = process.env.MONGO_SERVER_URL as string;
+  const user = JSON.parse(secret) as { username: string; password: string };
+  mongoConnectionString = `mongodb://${user.username}:${user.password}@${url}`;
+}
+
 const log = winston.createLogger({
-  level: 'info',
+  level: "info",
   format: combine(timestamp(), json()),
   transports: [
     new winston.transports.Console({ format: combine(timestamp(), simple()) }),
     new winston.transports.MongoDB({
-      db: process.env.MONGO_CONNECTION_STRING as string,
-      collection: 'log',
+      db: mongoConnectionString,
+      collection: "log",
     }),
   ],
 });

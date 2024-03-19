232e317d6e61e7ef5bf214eb63cfa690a9b90438
vincentsz <vincentsz@users.noreply.github.com>
2024-03-19 18:22:09 -0400

    Revert "implemented API endpoint for logging DB"
    This reverts commit d905310d7121cb48a68f8fc0e3e1696aaa282d0a.

--- ingress/src/ingress.ts
+++ ingress/src/ingress.ts
@@ -6,14 +6,6 @@ import { Event } from './types/types';
 import { addEvent } from './services/pg-service';
 
 import { v4 } from 'uuid';
-import { MongoClient } from 'mongodb';
-
-const uri = process.env.MONGO_URI ?? '';
-const dbName = process.env.MONGO_DB_NAME ?? '';
-
-const client = new MongoClient(uri);
-
-client.connect();
 
 export const app = express();
 app.use(express.json());
@@ -27,18 +19,6 @@ app.post('/events', (req: Request<{}, {}, Event>, res) => {
   addEvent({ ...req.body, id: v4() });
   res.status(200);
   return res.send();
-});
-
-app.get('/logs', async (req, res) => {
-  try {
-    const database = client.db(dbName);
-    const collection = database.collection('logs');
-    const logs = await collection.find({}).toArray();
-    res.status(200).json(logs);
-  } catch (error) {
-    console.error('Error retrieving logs from MongoDB:', error);
-    res.status(500).json({ error: 'Failed to retrieve logs from MongoDB' });
-  }
 });
 
 export default app;

d85d9bfdf66249fcd1d2e2c488db519f16599736
Christopher Perkins <Christopher.Perkins@null.net>
2024-03-24 12:52:30 -0400

    fix: functions output and example correct now

--- reverb-cli/src/commands/functions/index.ts
+++ reverb-cli/src/commands/functions/index.ts
@@ -2,19 +2,41 @@ import chalk from "chalk";
 import { ApiCommand } from "../../apiCommand.js";
 import { FuncStatus } from "../../types/types.js";
 import { getEmoji } from "../../utils/utils.js";
+import { Flags } from "@oclif/core";
 
 export default class Functions extends ApiCommand<typeof Functions> {
   static description = "Get functions that have occured within a time period";
 
   static examples = [
     `$ <%= config.bin %> functions -u https://example.com
-${chalk.greenBright(
-  "event1"
-)} | 28268a12-9668-4b92-ad77-9bfd8801222a | ${chalk.yellow(
-      "Sun, 24 Mar 2024 00:14:41 GMT"
-    )}
-`,
+[Success] Showing functions from 2024-03-17T06:56:05.332Z to 2024-03-24T06:56:05.332Z:
+
+🟢${chalk.greenBright(
+      "cron-function"
+    )} | f622f15c-d759-4455-88dc-63c51c748336 | ${chalk.yellowBright(
+      "Sun, 24 Mar 2024 06:24:00 GMT"
+    )} | ${chalk.yellow("Sun, 24 Mar 2024 06:24:00 GMT")}`,
   ];
+
+  static flags = {
+    apiUrl: Flags.string({
+      char: "u",
+      description: "The url to the api gateway for this call",
+      required: false,
+    }),
+    start: Flags.string({
+      char: "s",
+      description:
+        "Start Time for logs to get in a javascript parsable format(Defaults to 7 day prior to end)",
+      required: false,
+    }),
+    end: Flags.string({
+      char: "e",
+      description:
+        "End Time for logs to get in a javascript parsable format(Defaults to now)",
+      required: false,
+    }),
+  };
 
   async run(): Promise<void> {
     const url = await this.getUrl();
@@ -46,11 +68,12 @@ ${chalk.greenBright(
       const { funcId, funcName, lastUpdate, invoked, status } = fn;
       const emoji = getEmoji(status);
       const time = new Date(lastUpdate).toUTCString();
+      const inv = new Date(invoked).toUTCString();
 
       this.log(
         `${emoji}${chalk.greenBright(
           funcName
-        )} | ${funcId} | ${chalk.yellowBright(invoked)} | ${chalk.yellow(time)}`
+        )} | ${funcId} | ${chalk.yellowBright(inv)} | ${chalk.yellow(time)}`
       );
     }
   }

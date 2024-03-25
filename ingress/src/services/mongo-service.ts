7c0c5d05921c76bab93cbb57e694baeafd26072d
Morgan Davis <mdavisillustration@gmail.com>
2024-03-25 12:38:33 -0400

    chore: updated env var MONGO_URI to MONGO_CONNECTION_STRING

--- ingress/src/services/mongo-service.ts
+++ ingress/src/services/mongo-service.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGO_URI ?? '';
+const uri = process.env.MONGO_CONNECTION_STRING ?? '';
 const dbName = process.env.MONGO_DB_NAME ?? '';
 const client = new MongoClient(uri);
 client.connect();

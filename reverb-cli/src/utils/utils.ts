e72d82b272d4ba2367f476051f0eb49a23e60319
Christopher Perkins <Christopher.Perkins@null.net>
2024-04-17 13:04:51 -0400

    fix[cli] Updated CDK url to new one.

--- reverb-cli/src/utils/utils.ts
+++ reverb-cli/src/utils/utils.ts
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 import { promisify } from "util";
 
 export const GITHUB_ZIP =
-  "https://github.com/2401-Team-6/reverb-infrastructure/archive/refs/heads/master.zip";
+  "https://github.com/reverb-app/reverb-infrastructure/archive/refs/heads/master.zip";
 export const DIRECTORY_NAME = "reverb-infrastructure-master";
 export const runCommand = promisify(exec);
 
